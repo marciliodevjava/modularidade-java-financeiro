bec4e3dafc121a20b9bcb3c5fbd350b0d3789276
marciliodevjava <marciliodevjava@gmail.com>
2022-06-02 17:22:13 -0300

    Interface Calc1

---
+++ src/br/com/cod3r/app/financeiro/Calc1.java
@@ -0,0 +1,11 @@
+package br.com.cod3r.app.financeiro;
+
+public class Calc1 implements Calc{
+
+	@Override
+	public double soma(double... nums) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
