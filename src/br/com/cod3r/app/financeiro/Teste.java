c911c387d387f2c3ed8827164b46557357e6ecb9
marciliodevjava <marciliodevjava@gmail.com>
2022-06-02 17:29:02 -0300

    Atualização

--- src/br/com/cod3r/app/financeiro/Teste.java
+++ src/br/com/cod3r/app/financeiro/Teste.java
@@ -1,7 +1,6 @@
 package br.com.cod3r.app.financeiro;
 
 import java.lang.reflect.Field;
-
 import br.com.cod3r.app.calculo.Calculadora;
 import br.com.cod3r.app.calculo.interno.OperacoesAritmeticas;
 
@@ -15,23 +14,23 @@ public class Teste {
 		System.out.println(c1.soma(1,2,3));
 		System.out.println(c2.soma(1,2,3));
 		
-//		Calculadora calc = new Calculadora();
-//		System.out.println(calc.soma(2, 3, 4));
-//	
-//		OperacoesAritmeticas op = new OperacoesAritmeticas();
-//		System.out.println(op.soma(4,5,6));
-//		
-//		try {
-//			Field fieldId = Calculadora.class.getDeclaredFields()[0];
-//			fieldId.setAccessible(true);
-//			fieldId.set(calc, "def");
-//			fieldId.setAccessible(false);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		System.out.println(Calculadora.class.getName());
+		Calculadora calc = new Calculadora();
+		System.out.println(calc.soma(2, 3, 4));
+	
+		OperacoesAritmeticas op = new OperacoesAritmeticas();
+		System.out.println(op.soma(4,5,6));
+		
+		try {
+			Field fieldId = Calculadora.class.getDeclaredFields()[0];
+			fieldId.setAccessible(true);
+			fieldId.set(calc, "def");
+			fieldId.setAccessible(false);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(Calculadora.class.getName());
 	}
 	
 }
